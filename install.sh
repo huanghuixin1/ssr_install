24219d221d7b4c26b9f41085808617650f212094
hhx <249803023@qq.com>
2017-11-13 09:29:22 +0800

    修改换行为lf  修复开机自动启动的问题

--- install.sh
+++ install.sh
@@ -6,24 +6,28 @@ fi
 cd $sourcePath
 
 if [ -f '/etc/redhat-release'  ] ; then
-  yum install -y python git
+        yum update
+        yum install -y python git
 else
-  source /etc/os-release
-  cd "$sourcePath"
-  if [ "$ID" = "debian" ] || [ "$ID" = "ubuntu" ] ; then
-      apt-get install -y  python git
-  elif [ "$ID" = "centos" ]; then
-      yum install -y  python git
-  else
-      echo "shit 系统不存在  到github给我提issue "
-      echo "https://github.com/huanghuixin1/shadowsocksr.git"
-  fi
+        source /etc/os-release
+        cd "$sourcePath"
+        if [ "$ID" = "debian" ] || [ "$ID" = "ubuntu" ] ; then
+                apt-get update
+                apt-get install -y  python git
+        elif [ "$ID" = "centos" ]; then
+                yum update
+                yum install -y  python git
+        else
+                echo "shit ϵͳ������  ��github������issue "
+		echo "https://github.com/huanghuixin1/shadowsocksr.git"
+        fi
 fi
 
 git clone https://github.com/huanghuixin1/shadowsocksr.git
 python /usr/local/ssr_hhx/shadowsocksr/shadowsocks/server.py -p 8899 -k 52hhx.com -m none -O auth_chain_a -o tls1.2_ticket_auth_compatible &
 
 chmod +x /etc/rc.local
-sed -i '1i\python /usr/local/ssr_hhx/shadowsocksr/shadowsocks/server.py -p 8899 -k 52hhx.com -m none -O auth_chain_a -o tls1.2_ticket_auth_compatible &' /etc/rc.local
+# 倒数第二行添加
+sed -i '$i\python /usr/local/ssr_hhx/shadowsocksr/shadowsocks/server.py -p 8899 -k hhx6688 -m none -O auth_chain_a -o tls1.2_ticket_auth_compatible &' /etc/rc.loca
 
 exit 0
