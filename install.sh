bfc7fc19f530fbd1ecef8bac66fabab225e82f6a
hhx <249803023@qq.com>
2018-05-04 15:41:10 +0800

    增加开机自动启动服务

--- install.sh
+++ install.sh
@@ -29,5 +29,5 @@ python /usr/local/ssr_hhx/shadowsocksr/shadowsocks/server.py -p 8899 -k 52hhx.co
 chmod +x /etc/rc.local
 # 倒数第二行添加
 sed -i '$i\python /usr/local/ssr_hhx/shadowsocksr/shadowsocks/server.py -p 8899 -k 52hhx.com -m none -O auth_chain_a -o tls1.2_ticket_auth_compatible &' /etc/rc.local
-
+systemctl enable rc-local
 exit 0
